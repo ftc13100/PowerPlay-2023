99401760741fdf945affdd6dd4c4f3e51defe25f
SachetK <71898487+SachetK@users.noreply.github.com>
2022-09-14 08:47:55 -0400

    Test Conditional Command for TriggerReader

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode.opModes.teleOp
 
 import com.arcrobotics.ftclib.command.CommandOpMode
+import com.arcrobotics.ftclib.command.ConditionalCommand
 import com.arcrobotics.ftclib.gamepad.GamepadEx
+import com.arcrobotics.ftclib.gamepad.GamepadKeys
 import com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.A
 import com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.B
+import com.arcrobotics.ftclib.gamepad.GamepadKeys.Trigger
+import com.arcrobotics.ftclib.gamepad.GamepadKeys.Trigger.*
+import com.arcrobotics.ftclib.gamepad.TriggerReader
 import com.arcrobotics.ftclib.hardware.motors.Motor
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.commands.IntakeCommand
@@ -17,5 +22,12 @@ class MainTeleOp: CommandOpMode() {
     override fun initialize() {
         driver.getGamepadButton(A).whenHeld(intakeCommand)
         driver.getGamepadButton(B).whenHeld(outtakeCommand)
+        schedule(
+            ConditionalCommand(
+                intakeCommand,
+                outtakeCommand,
+                TriggerReader(driver, LEFT_TRIGGER)::isDown
+            )
+        )
     }
 }
