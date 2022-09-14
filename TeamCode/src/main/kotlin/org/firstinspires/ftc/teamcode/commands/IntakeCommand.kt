8b00488735c41c4f41c85bf1eed7e2031ecfa716
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-09-14 00:39:34 -0400

    Make functions more succinct using function expression syntax

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/IntakeCommand.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/IntakeCommand.kt
@@ -4,14 +4,6 @@ import com.arcrobotics.ftclib.command.CommandBase
 import com.arcrobotics.ftclib.hardware.motors.Motor
 
 class IntakeCommand(private val motor: Motor, private val intake: Boolean) : CommandBase() {
-    override fun execute() {
-        motor.set(
-            if(intake) 1.0
-            else -1.0
-        )
-    }
-
-    override fun end(interrupted: Boolean) {
-        motor.set(0.0)
-    }
+    override fun execute() = motor.set(if (intake) 1.0 else -1.0)
+    override fun end(interrupted: Boolean) = motor.set(0.0)
 }
