f762e367b5c20b0dcb2f6d70d7fc572816f2ea88
SachetK <71898487+SachetK@users.noreply.github.com>
2022-09-15 22:18:04 -0400

    create basic slide subsystem frame

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/constants/DeviceConfig.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/constants/DeviceConfig.kt
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.constants
 
 enum class DeviceConfig(val deviceName: String) {
-    INTAKE_MOTOR("intake motor")
+    INTAKE_MOTOR("intake"),
+    SLIDES_MOTOR_LEFT("leftSlide"),
+    SLIDES_MOTOR_RIGHT("rightSlide")
 }
---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.kt
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+import com.arcrobotics.ftclib.command.SubsystemBase
+import com.arcrobotics.ftclib.hardware.motors.MotorGroup
+
+class SlidesSubsystem(private val slideMotors: MotorGroup): SubsystemBase() {
+
+}
