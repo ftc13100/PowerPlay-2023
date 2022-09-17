727f133bec5b21ca94691d794e439e134287e51d
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-09-16 21:21:13 -0400

    Added camera property config for camera

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/constants/AprilTagCamera.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.constants
+
+// Microsoft Lifecam HD-3000
+enum class AprilTagCamera(val value: Double) {
+    // TODO("Measure Tagsize")
+    TAGSIZE(0.0),
+    FX(678.154),
+    FY(678.17),
+    CX(318.135),
+    CY(228.374)
+}
