197cd2a7db132d330df1642ecc8b0e083ab9ed27
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-09-16 21:18:32 -0400

    Implemented a vision subsystem

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/vision/VisionSubsystem.kt
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.subsystems.vision
+
+import com.arcrobotics.ftclib.command.SubsystemBase
+import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
+import org.firstinspires.ftc.teamcode.subsystems.vision.pipelines.OpenCvResultsPipeline
+import org.openftc.easyopencv.OpenCvCamera
+import org.openftc.easyopencv.OpenCvCamera.AsyncCameraOpenListener
+import org.openftc.easyopencv.OpenCvCameraFactory
+import org.openftc.easyopencv.OpenCvCameraRotation
+
+class VisionSubsystem<T>(webcam: WebcamName, cameraMonitorViewId: Int, pipeline: OpenCvResultsPipeline<T>, private val telemetry: Telemetry) : SubsystemBase() {
+    private val camera: OpenCvCamera
+    init {
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcam, cameraMonitorViewId)
+
+        camera.openCameraDeviceAsync(object: AsyncCameraOpenListener {
+            override fun onOpened() {
+                camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT)
+            }
+
+            override fun onError(errorCode: Int) {
+                telemetry.addData("Error", "Camera error $errorCode")
+            }
+        })
+
+        camera.setPipeline(pipeline)
+    }
+}
