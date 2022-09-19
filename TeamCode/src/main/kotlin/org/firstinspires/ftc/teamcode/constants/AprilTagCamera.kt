a10123e95f98bd591b936b91b96784f3ccaf7327
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-09-18 21:58:43 -0400

    Add constants for Lifecam HD-3000

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/constants/AprilTagCamera.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/constants/AprilTagCamera.kt
@@ -2,8 +2,7 @@ package org.firstinspires.ftc.teamcode.constants
 
 // Microsoft Lifecam HD-3000
 enum class AprilTagCamera(val value: Double) {
-    // TODO("Measure Tagsize")
-    TAGSIZE(0.0),
+    TAGSIZE(0.0415),
     FX(678.154),
     FY(678.17),
     CX(318.135),
