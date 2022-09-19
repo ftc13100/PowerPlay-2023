83ddfaab8b96340f9cc36f7c313cec716e39ec67
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-09-18 21:56:37 -0400

    Remove Vision Subsystem

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/vision/pipelines/OpenCvResultsPipeline.kt
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.subsystems.vision.pipelines
-
-import org.openftc.easyopencv.OpenCvPipeline
-
-abstract class OpenCvResultsPipeline<T> : OpenCvPipeline() {
-    abstract fun getLatestResults(): List<T>
-}
