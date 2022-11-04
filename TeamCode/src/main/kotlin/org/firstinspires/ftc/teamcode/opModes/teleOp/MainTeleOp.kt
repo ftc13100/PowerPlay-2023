f7cd7ea91b8b272ddb2d8242b47d16f8a361e3bb
SachetK <71898487+SachetK@users.noreply.github.com>
2022-11-04 18:40:19 -0400

    TeleOp for stinky putreach

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
@@ -1,11 +1,22 @@
 package org.firstinspires.ftc.teamcode.opModes.teleOp
 
 import com.arcrobotics.ftclib.command.CommandOpMode
+import com.arcrobotics.ftclib.gamepad.GamepadEx
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.commands.DriveCommand
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem
 
 @TeleOp(name = "Main")
-class MainTeleOp : CommandOpMode(){
+class MainTeleOp : CommandOpMode() {
     override fun initialize() {
-        TODO("Not yet implemented")
+        val driveSubsystem = DriveSubsystem(SampleMecanumDrive(hardwareMap), false)
+
+        val driver = GamepadEx(gamepad1)
+
+        val driveCommand = DriveCommand(driveSubsystem, driver::getLeftX, driver::getLeftY, driver::getRightX, 0.15)
+
+        register(driveSubsystem)
+        driveSubsystem.defaultCommand = driveCommand
     }
 }
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/tests/DriveTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/tests/DriveTeleOp.kt
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.opModes.teleOp.tests
 
 import com.arcrobotics.ftclib.command.CommandOpMode
 import com.arcrobotics.ftclib.command.ConditionalCommand
-import com.arcrobotics.ftclib.command.Subsystem
 import com.arcrobotics.ftclib.gamepad.GamepadEx
 import com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.A
 import com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.B
@@ -12,30 +11,30 @@ import com.arcrobotics.ftclib.hardware.motors.Motor
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.commands.DriveCommand
 import org.firstinspires.ftc.teamcode.commands.IntakeCommand
-import org.firstinspires.ftc.teamcode.constants.DeviceConfig
+import org.firstinspires.ftc.teamcode.constants.DeviceConfig.INTAKE_MOTOR
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem
 
 @TeleOp(name = "Drive w/ Intake ", group = "Test")
 class DriveTeleOp : CommandOpMode() {
+    private val driver = GamepadEx(gamepad1)
+
+    private val intakeMotor = Motor(hardwareMap, INTAKE_MOTOR.deviceName)
+
+    private val intakeSubsystem = IntakeSubsystem(intakeMotor)
+    private val driveSubsystem = DriveSubsystem(SampleMecanumDrive(hardwareMap), false)
+
+    private val intakeCommand = IntakeCommand(intakeSubsystem, true)
+    private val outtakeCommand = IntakeCommand(intakeSubsystem, false)
+
+    private val driveCommand = DriveCommand(driveSubsystem, driver::getLeftX, driver::getLeftY, driver::getRightX, 0.15)
+
     override fun initialize() {
-        val intakeMotor = Motor(hardwareMap, DeviceConfig.INTAKE_MOTOR.deviceName)
-
-        val intakeSubsystem = IntakeSubsystem(intakeMotor)
-        val driveSubsystem = DriveSubsystem(SampleMecanumDrive(hardwareMap), false)
-
-        val intakeCommand = IntakeCommand(intakeSubsystem, true)
-        val outtakeCommand = IntakeCommand(intakeSubsystem, false)
-
-        val driver = GamepadEx(gamepad1)
-
-        val driveCommand = DriveCommand(driveSubsystem, driver::getLeftX, driver::getLeftY, driver::getRightX, 0.15)
-
         driver.getGamepadButton(A).whenHeld(intakeCommand)
         driver.getGamepadButton(B).whenHeld(outtakeCommand)
 
-        register(driveSubsystem as Subsystem)
+        register(driveSubsystem)
 
         schedule(
             ConditionalCommand(intakeCommand, outtakeCommand, TriggerReader(driver, LEFT_TRIGGER)::isDown),
