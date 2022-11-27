438cfc7c6eb057246f1a2e10de7e1dc2320617bc
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-11-27 18:20:12 -0500

    Some stuff

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/OpenElevatorSubsystem.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/OpenElevatorSubsystem.kt
@@ -4,15 +4,25 @@ import com.arcrobotics.ftclib.command.SubsystemBase
 import com.arcrobotics.ftclib.hardware.motors.Motor
 import com.arcrobotics.ftclib.hardware.motors.MotorGroup
 import com.qualcomm.robotcore.hardware.TouchSensor
+import org.firstinspires.ftc.robotcore.external.Telemetry
 
-class OpenElevatorSubsystem(leftMotor: Motor, rightMotor: Motor, private val limit: TouchSensor): SubsystemBase() {
+class OpenElevatorSubsystem(leftMotor: Motor, rightMotor: Motor, private val limit: TouchSensor, val telemetry: Telemetry): SubsystemBase() {
     private val elevatorMotors = MotorGroup(leftMotor, rightMotor)
-
+    private var maxVel = 0.0
     init {
         elevatorMotors.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE)
     }
 
-    fun spinUp() = elevatorMotors.set(1.0)
+    fun spinUp() {
+        elevatorMotors.set(1.0)
+        val current = elevatorMotors.velocities.first()
+        maxVel = current.coerceAtLeast(maxVel)
+        telemetry.addData("Velocity", current)
+        telemetry.update()
+
+    }
+
+    fun getPosition(): Double = elevatorMotors.positions.first()
 
     fun spinDown() = elevatorMotors.set(-1.0)
 
