95ea95f889c92b0e6deeb827a0bda7e6ded0d2be
SachetK <71898487+SachetK@users.noreply.github.com>
2022-12-04 17:54:40 -0500

    Convert fields to lateinit

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
@@ -21,32 +21,32 @@ import org.firstinspires.ftc.teamcode.subsystems.SlidesSubsystem
 @TeleOp(name = "Main")
 class MainTeleOp : CommandOpMode() {
     // Hardware
-    private var slidesLeft: Motor? = null
-    private var slidesRight: Motor? = null
-    private var limit: TouchSensor? = null
+    private lateinit var slidesLeft: Motor
+    private lateinit var slidesRight: Motor
+    private lateinit var limit: TouchSensor
 
-    private var intakeMotor: Motor? = null
+    private lateinit var intakeMotor: Motor
 
     // Subsystems
-    private var driveSubsystem: DriveSubsystem? = null
-    private var slidesSubsystem: SlidesSubsystem? = null
-    private var intakeSubsystem: IntakeSubsystem? = null
+    private lateinit var driveSubsystem: DriveSubsystem
+    private lateinit var slidesSubsystem: SlidesSubsystem
+    private lateinit var intakeSubsystem: IntakeSubsystem
 
     // Commands
-    private var driveCommand: DriveCommand? = null
+    private lateinit var driveCommand: DriveCommand
 
-    private var slidesGroundCommand: SlidesCommand? = null
-    private var slidesIntakeCommand: SlidesCommand? = null
-    private var slidesLowCommand: SlidesCommand? = null
-    private var slidesMidCommand: SlidesCommand? = null
-    private var slidesHighCommand: SlidesCommand? = null
+    private lateinit var slidesGroundCommand: SlidesCommand
+    private lateinit var slidesIntakeCommand: SlidesCommand
+    private lateinit var slidesLowCommand: SlidesCommand
+    private lateinit var slidesMidCommand: SlidesCommand
+    private lateinit var slidesHighCommand: SlidesCommand
 
-    private var intakeCommand: IntakeCommand? = null
-    private var outtakeCommand: IntakeCommand? = null
+    private lateinit var intakeCommand: IntakeCommand
+    private lateinit var outtakeCommand: IntakeCommand
 
     // Gamepads
-    private var driver: GamepadEx? = null
-    private var operator: GamepadEx? = null
+    private lateinit var driver: GamepadEx
+    private lateinit var operator: GamepadEx
 
     override fun initialize() {
         // Debug
@@ -61,47 +61,44 @@ class MainTeleOp : CommandOpMode() {
 
         // Subsystems
         driveSubsystem = DriveSubsystem(SampleMecanumDrive(hardwareMap), false)
-        slidesSubsystem = SlidesSubsystem(slidesLeft!!, slidesRight!!, limit!!, telemetry)
-        intakeSubsystem = IntakeSubsystem(intakeMotor!!)
+        slidesSubsystem = SlidesSubsystem(slidesLeft, slidesRight, limit, telemetry)
+        intakeSubsystem = IntakeSubsystem(intakeMotor)
 
         // Gamepads
         driver = GamepadEx(gamepad1)
         operator = GamepadEx(gamepad2)
 
         // Commands
-        driveCommand = DriveCommand(driveSubsystem!!, driver!!::getLeftX, driver!!::getLeftY, driver!!::getRightX, 0.15)
+        driveCommand = DriveCommand(driveSubsystem, driver::getLeftX, driver::getLeftY, driver::getRightX, 0.15)
 
-        slidesGroundCommand = SlidesCommand(slidesSubsystem!!, SlidesConst.SlidesPosition.GROUND)
-        slidesIntakeCommand = SlidesCommand(slidesSubsystem!!, SlidesConst.SlidesPosition.INTAKE)
-        slidesLowCommand = SlidesCommand(slidesSubsystem!!, SlidesConst.SlidesPosition.LOW)
-        slidesMidCommand = SlidesCommand(slidesSubsystem!!, SlidesConst.SlidesPosition.MIDDLE)
-        slidesHighCommand = SlidesCommand(slidesSubsystem!!, SlidesConst.SlidesPosition.HIGH)
+        slidesGroundCommand = SlidesCommand(slidesSubsystem, SlidesConst.SlidesPosition.GROUND)
+        slidesIntakeCommand = SlidesCommand(slidesSubsystem, SlidesConst.SlidesPosition.INTAKE)
+        slidesLowCommand = SlidesCommand(slidesSubsystem, SlidesConst.SlidesPosition.LOW)
+        slidesMidCommand = SlidesCommand(slidesSubsystem, SlidesConst.SlidesPosition.MIDDLE)
+        slidesHighCommand = SlidesCommand(slidesSubsystem, SlidesConst.SlidesPosition.HIGH)
 
-        intakeCommand = IntakeCommand(intakeSubsystem!!, true)
-        outtakeCommand = IntakeCommand(intakeSubsystem!!, false)
+        intakeCommand = IntakeCommand(intakeSubsystem, true)
+        outtakeCommand = IntakeCommand(intakeSubsystem, false)
 
         // Assign commands to gamepads
-        driver!!.getGamepadButton(GamepadKeys.Button.X).whenPressed(slidesGroundCommand)
-        driver!!.getGamepadButton(GamepadKeys.Button.B).whenPressed(slidesIntakeCommand)
+        driver.getGamepadButton(GamepadKeys.Button.X).whenPressed(slidesGroundCommand)
+        driver.getGamepadButton(GamepadKeys.Button.B).whenPressed(slidesIntakeCommand)
 
-        driver!!.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whileHeld(intakeCommand)
-        driver!!.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whileHeld(intakeCommand)
+        driver.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whileHeld(intakeCommand)
+        driver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whileHeld(intakeCommand)
 
-        operator!!.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whileHeld(outtakeCommand)
-        operator!!.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whileHeld(outtakeCommand)
+        operator.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whileHeld(outtakeCommand)
+        operator.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whileHeld(outtakeCommand)
 
-        operator!!.getGamepadButton(GamepadKeys.Button.X).whenPressed(slidesGroundCommand)
-        operator!!.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whenPressed(slidesLowCommand)
-        operator!!.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenPressed(slidesMidCommand)
-        operator!!.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(slidesHighCommand)
-
-
+        operator.getGamepadButton(GamepadKeys.Button.X).whenPressed(slidesGroundCommand)
+        operator.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whenPressed(slidesLowCommand)
+        operator.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenPressed(slidesMidCommand)
+        operator.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(slidesHighCommand)
 
         // Register Subsystems
         register(driveSubsystem)
 
         // Assign Default Commands
-        driveSubsystem!!.defaultCommand = driveCommand
-
+        driveSubsystem.defaultCommand = driveCommand
     }
 }
