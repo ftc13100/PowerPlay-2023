c30a89a9af7bfb7e298550bf53df2dcde18be8bd
SachetK <71898487+SachetK@users.noreply.github.com>
2022-12-07 22:05:46 -0500

    Add timeouts to slides command so that the adjust height command can be used

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/slides/HeightCommand.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/slides/HeightCommand.kt
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.commands.slides
 
 import com.arcrobotics.ftclib.command.CommandBase
+import org.firstinspires.ftc.teamcode.constants.SlidesConst
 import org.firstinspires.ftc.teamcode.subsystems.SlidesSubsystem
 import java.util.function.DoubleSupplier
 
@@ -15,5 +16,11 @@ class HeightCommand(
     override fun execute() {
         subsystem.increaseTargetPosition(increase.asDouble * 5)
         subsystem.operateSlides()
+
+        if (subsystem.getTargetPosition() == SlidesConst.SlidesPosition.GROUND && subsystem.atTargetPosition() || subsystem.isPressed()) {
+            subsystem.stop()
+        } else if (subsystem.atTargetPosition()) {
+            subsystem.stall()
+        }
     }
 }
