fb313567907c9651b123e910d93557897cb8f223
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-12-08 23:57:52 -0500

    Operate the slides in execute, and assign a finish and end

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/slides/HeightCommand.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/slides/HeightCommand.kt
@@ -7,12 +7,18 @@ import java.util.function.DoubleSupplier
 class HeightCommand(
     private val subsystem: SlidesSubsystem,
     private val increase: DoubleSupplier
-): CommandBase() {
+) : CommandBase() {
+
     init {
         addRequirements(subsystem)
     }
 
     override fun execute() {
         subsystem.increaseTargetPosition(increase.asDouble * 10)
+        subsystem.operateSlides()
     }
+
+    override fun isFinished(): Boolean = subsystem.atTargetPosition() && increase.asDouble == 0.0
+
+    override fun end(interrupted: Boolean) = subsystem.stall()
 }
