5f740d54b8ce3e9decc1f41724b0b75c1eab0f17
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-12-17 00:45:03 -0500

    Modify VisionTeleOp to test the new pipeline

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/tests/VisionTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/tests/VisionTeleOp.kt
@@ -7,16 +7,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
-import org.firstinspires.ftc.teamcode.constants.AprilTagCamera
 import org.firstinspires.ftc.teamcode.constants.DeviceConfig
-import org.firstinspires.ftc.teamcode.subsystems.vision.pipelines.AprilTagDetectionPipeline
+import org.firstinspires.ftc.teamcode.subsystems.vision.pipelines.JunctionDetectionPipeline
 import org.openftc.easyopencv.OpenCvCamera
 import org.openftc.easyopencv.OpenCvCamera.AsyncCameraOpenListener
 import org.openftc.easyopencv.OpenCvCameraFactory
 import org.openftc.easyopencv.OpenCvCameraRotation
 
 @Disabled
-@TeleOp(name = "Vision Test", group = "Tests")
+@TeleOp(name = "Junction Vision Test", group = "Tests")
 class VisionTeleOp : LinearOpMode() {
     @SuppressLint("DiscouragedApi")
     override fun runOpMode() {
@@ -30,13 +29,7 @@ class VisionTeleOp : LinearOpMode() {
         )
         val webcamName: WebcamName = hardwareMap.get(WebcamName::class.java, DeviceConfig.VISION_CAMERA.deviceName)
         val camera: OpenCvCamera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, monitorId)
-        val pipeline = AprilTagDetectionPipeline(
-            AprilTagCamera.TAGSIZE.value,
-            AprilTagCamera.FX.value,
-            AprilTagCamera.FY.value,
-            AprilTagCamera.CX.value,
-            AprilTagCamera.CY.value
-        )
+        val pipeline = JunctionDetectionPipeline()
 
         camera.setPipeline(pipeline)
 
@@ -54,20 +47,7 @@ class VisionTeleOp : LinearOpMode() {
         waitForStart()
 
         while (opModeIsActive()) {
-            if (pipeline.getLatestResults().isEmpty()) continue
-
-            val elementId = pipeline.getLatestResults()[0].id
-            telemetry.addData("Tag ID", elementId)
-
-            val zone = when (elementId) {
-                1021 -> "Zone 1"
-                1213 -> "Zone 2"
-                302 -> "Zone 3"
-                else -> ""
-            }
-            telemetry.addData("Zone", zone)
-
-            telemetry.update()
+            continue
         }
     }
 }
