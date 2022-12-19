5232b71d87d43993c60439a5e67c705d1148ce4a
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-12-19 09:25:10 -0500

    Add basic pipeline to detect junction

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/vision/pipelines/JunctionDetectionPipeline.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/vision/pipelines/JunctionDetectionPipeline.kt
@@ -1,21 +1,24 @@
 package org.firstinspires.ftc.teamcode.subsystems.vision.pipelines
 
-import org.opencv.core.Mat
-import org.opencv.core.Point
-import org.opencv.core.Rect
-import org.opencv.core.Scalar
+import com.acmerobotics.dashboard.config.Config
+import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 import org.openftc.easyopencv.OpenCvPipeline
 
+@Config
 class JunctionDetectionPipeline : OpenCvPipeline() {
-    private val gray = Mat()
+    private val mat = Mat()
     private val submat = Rect(Point(0.0, 190.0), Point(639.0, 290.0))
 
+    val highBound = Scalar(127.0, 220.0, 255.0)
+    val lowBound = Scalar(5.0, 30.0, 220.0)
     override fun processFrame(input: Mat?): Mat {
-        Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGB2HSV)
-        Imgproc.rectangle(gray, submat, Scalar(255.0, 0.0, 0.0))
 
-        return gray
+        Imgproc.GaussianBlur(input, mat, Size(19.0, 19.0), 0.0);
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HLS)
+        Core.inRange(mat, lowBound, highBound, mat)
+        Imgproc.rectangle(mat, submat, Scalar(255.0, 0.0, 0.0))
+
+        return mat
     }
-
 }
