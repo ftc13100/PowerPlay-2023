c167914670003df00a27a1286dd26552ca1f0d9a
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-12-19 19:23:27 -0500

    Add some morphology open and close and contour detection

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/vision/pipelines/JunctionDetectionPipeline.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/vision/pipelines/JunctionDetectionPipeline.kt
@@ -5,19 +5,26 @@ import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 import org.openftc.easyopencv.OpenCvPipeline
 
-@Config
+
 class JunctionDetectionPipeline : OpenCvPipeline() {
-    private val mat = Mat()
+    private var mat = Mat()
+    private var contours: List<MatOfPoint> = ArrayList()
+
     private val submat = Rect(Point(0.0, 190.0), Point(639.0, 290.0))
+    private val size = Size(19.0, 19.0)
+    private val lowBound = Scalar(11.0, 115.0, 55.0)
+    private val highBound = Scalar(34.0, 255.0, 255.0)
+    private val kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, Size(17.0, 17.0))
+    private val red = Scalar(255.0, 0.0, 0.0)
 
-    val highBound = Scalar(127.0, 220.0, 255.0)
-    val lowBound = Scalar(5.0, 30.0, 220.0)
     override fun processFrame(input: Mat?): Mat {
-
-        Imgproc.GaussianBlur(input, mat, Size(19.0, 19.0), 0.0);
+        Imgproc.GaussianBlur(input, mat, size, 0.0)
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HLS)
         Core.inRange(mat, lowBound, highBound, mat)
-        Imgproc.rectangle(mat, submat, Scalar(255.0, 0.0, 0.0))
+        Imgproc.morphologyEx(mat, mat, Imgproc.MORPH_CLOSE, kernel)
+        Imgproc.morphologyEx(mat, mat, Imgproc.MORPH_OPEN, kernel)
+        Imgproc.findContours(mat, contours, null, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE)
+        Imgproc.rectangle(mat, submat, red)
 
         return mat
     }
