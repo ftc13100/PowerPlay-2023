10b817aee252fcd8c58fdbda330ad6e65e9ced4a
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-12-22 21:53:50 -0500

    Add a servo for rotating the claw

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/ClawIntakeSubsystem.kt
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+import com.arcrobotics.ftclib.command.SubsystemBase
+import com.qualcomm.robotcore.hardware.Servo
+
+// For the sake of consistency, kind of useless, but allows for adaptability later on
+class ClawIntakeSubsystem(private val clawServo: Servo, private val rotationServo: Servo) : SubsystemBase() {
+    fun openClaw() { clawServo.position = 1.0 }
+
+    fun closeClaw() { clawServo.position = 0.0 }
+
+    fun rotateLeft() { rotationServo.position = 0.0 }
+
+    fun rotateMid() { rotationServo.position = 0.5 }
+
+    fun rotateRight() { rotationServo.position = 1.0 }
+}
