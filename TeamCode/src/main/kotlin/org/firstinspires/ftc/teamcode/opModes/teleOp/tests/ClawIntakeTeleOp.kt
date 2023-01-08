0e710e8883da1d3a4b2409051dffe4350f29ee31
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2023-01-08 18:21:35 -0500

    Get rid of the sensor code and replace sensor trigger with RevColorSensorV3

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/tests/ClawIntakeTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/tests/ClawIntakeTeleOp.kt
@@ -7,29 +7,21 @@ import com.arcrobotics.ftclib.command.InstantCommand
 import com.arcrobotics.ftclib.gamepad.GamepadEx
 import com.arcrobotics.ftclib.gamepad.GamepadKeys
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.hardware.ColorSensor
-import com.qualcomm.robotcore.hardware.DistanceSensor
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.constants.DeviceConfig
 import org.firstinspires.ftc.teamcode.subsystems.ClawIntakeSubsystem
-import org.firstinspires.ftc.teamcode.triggers.ClawSensorTrigger
 
 @TeleOp(name = "Main")
 class ClawIntakeTeleOp : CommandOpMode() {
     // Hardware
     private lateinit var clawServo: Servo
     private lateinit var rotationServo: Servo
-    private lateinit var colorSensor: ColorSensor
-    private lateinit var distanceSensor: DistanceSensor
 
     // Subsystems
     private lateinit var intakeSubsystem: ClawIntakeSubsystem
 
     // Gamepads
     private lateinit var driver: GamepadEx
-
-    // Custom Triggers
-    private lateinit var clawSensorTrigger: ClawSensorTrigger
 
     override fun initialize() {
         // Debug
@@ -38,17 +30,12 @@ class ClawIntakeTeleOp : CommandOpMode() {
         // Hardware
         clawServo = hardwareMap.get(Servo::class.java, DeviceConfig.CLAW_SERVO.deviceName)
         rotationServo = hardwareMap.get(Servo::class.java, DeviceConfig.ROTATION_SERVO.deviceName)
-        colorSensor = hardwareMap.get(ColorSensor::class.java, DeviceConfig.COLOR_SENSOR.deviceName)
-        distanceSensor = hardwareMap.get(DistanceSensor::class.java, DeviceConfig.COLOR_SENSOR.deviceName)
 
         // Subsystems
         intakeSubsystem = ClawIntakeSubsystem(clawServo, rotationServo)
 
         // Gamepads
         driver = GamepadEx(gamepad1)
-
-        // Custom Triggers
-        clawSensorTrigger = ClawSensorTrigger(colorSensor, distanceSensor)
 
         // Assign commands to gamepads
         driver.getGamepadButton(GamepadKeys.Button.X).whenPressed(
@@ -80,14 +67,6 @@ class ClawIntakeTeleOp : CommandOpMode() {
         )
 
         driver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenPressed(
-            InstantCommand(
-                { intakeSubsystem.closeClaw() },
-                intakeSubsystem
-            )
-        )
-
-
-        clawSensorTrigger.whenActive(
             InstantCommand(
                 { intakeSubsystem.closeClaw() },
                 intakeSubsystem
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/triggers/ClawSensorTrigger.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/triggers/ClawSensorTrigger.kt
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode.triggers
 
 import com.arcrobotics.ftclib.command.button.Trigger
-import com.qualcomm.robotcore.hardware.ColorSensor
-import com.qualcomm.robotcore.hardware.DistanceSensor
+import com.qualcomm.hardware.rev.RevColorSensorV3
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 
-class ClawSensorTrigger(private val color: ColorSensor, private val distance: DistanceSensor) : Trigger() {
+class ClawSensorTrigger(private val colorSensor: RevColorSensorV3) : Trigger() {
     override fun get(): Boolean {
-        return color.red() > 0.5 && distance.getDistance(DistanceUnit.CM) < 5.0
+        return colorSensor.red() > 0.5 && colorSensor.getDistance(DistanceUnit.CM) < 5.0
     }
 }
