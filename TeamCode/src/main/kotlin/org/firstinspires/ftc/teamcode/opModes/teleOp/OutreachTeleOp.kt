30e50b7ccade8a7fe3e5711f7c5f1e8f1a204279
SachetK <71898487+SachetK@users.noreply.github.com>
2023-04-24 14:53:13 -0400

    Outreach TeleOp

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/OutreachTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/OutreachTeleOp.kt
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.opModes.teleOp
 
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
-import com.arcrobotics.ftclib.command.*
+import com.arcrobotics.ftclib.command.CommandOpMode
+import com.arcrobotics.ftclib.command.ConditionalCommand
+import com.arcrobotics.ftclib.command.InstantCommand
 import com.arcrobotics.ftclib.gamepad.GamepadEx
 import com.arcrobotics.ftclib.gamepad.GamepadKeys
 import com.arcrobotics.ftclib.hardware.motors.Motor
@@ -10,7 +12,6 @@ import com.qualcomm.hardware.rev.RevColorSensorV3
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.hardware.TouchSensor
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import org.firstinspires.ftc.teamcode.commands.drive.DriveCommand
 import org.firstinspires.ftc.teamcode.commands.slides.SpinCommand
 import org.firstinspires.ftc.teamcode.constants.DeviceConfig
@@ -19,15 +20,13 @@ import org.firstinspires.ftc.teamcode.constants.SlidesConst
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem
 import org.firstinspires.ftc.teamcode.subsystems.SlidesClawSubsystem
-import org.firstinspires.ftc.teamcode.triggers.ClawSensorTrigger
 
-@TeleOp(name = "Main (Open Loop)")
-class OpenMainTeleOp : CommandOpMode() {
+@TeleOp(name = "Main (Outreach & One Controller)")
+class OutreachTeleOp: CommandOpMode() {
     // Hardware
     private lateinit var slidesLeft: Motor
     private lateinit var slidesRight: Motor
     private lateinit var limit: TouchSensor
-    private lateinit var colorSensor: RevColorSensorV3
     private lateinit var clawServo: Servo
     private lateinit var rotationServo: Servo
 
@@ -37,7 +36,6 @@ class OpenMainTeleOp : CommandOpMode() {
 
     // Gamepads
     private lateinit var driver: GamepadEx
-    private lateinit var operator: GamepadEx
 
     // Commands
     private lateinit var driveCommand: DriveCommand
@@ -48,7 +46,6 @@ class OpenMainTeleOp : CommandOpMode() {
     private lateinit var openClawCommand: InstantCommand
     private lateinit var closeClawCommand: InstantCommand
 
-    private lateinit var clawTrigger: ClawSensorTrigger
 
     override fun initialize() {
         // Debug
@@ -60,7 +57,6 @@ class OpenMainTeleOp : CommandOpMode() {
         clawServo = hardwareMap.get(Servo::class.java, DeviceConfig.CLAW_SERVO.deviceName)
         rotationServo = hardwareMap.get(Servo::class.java, DeviceConfig.ROTATION_SERVO.deviceName)
         limit = hardwareMap.get(TouchSensor::class.java, DeviceConfig.SLIDES_LIMIT.deviceName)
-        colorSensor = hardwareMap.get(RevColorSensorV3::class.java, DeviceConfig.COLOR_SENSOR.deviceName)
 
         // Subsystems
         driveSubsystem = DriveSubsystem(SampleMecanumDrive(hardwareMap), false)
@@ -68,7 +64,6 @@ class OpenMainTeleOp : CommandOpMode() {
 
         // Gamepads
         driver = GamepadEx(gamepad1)
-        operator = GamepadEx(gamepad2)
 
         // Commands
         driveCommand = DriveCommand(driveSubsystem,
@@ -91,16 +86,13 @@ class OpenMainTeleOp : CommandOpMode() {
             slidesClawSubsystem.clawState = SlidesConst.ClawState.CLOSE
         }, slidesClawSubsystem)
 
-        // Custom Triggers
-        clawTrigger = ClawSensorTrigger(colorSensor, telemetry)
-
         driveSubsystem.poseEstimate = PoseStorage.poseEstimate
 
         // Assign commands to gamepads
-        operator.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whileHeld(spinDownCommand)
-        operator.getGamepadButton(GamepadKeys.Button.DPAD_UP).whileHeld(spinUpCommand)
+        driver.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whileHeld(spinDownCommand)
+        driver.getGamepadButton(GamepadKeys.Button.DPAD_UP).whileHeld(spinUpCommand)
 
-        operator.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenPressed(
+        driver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenPressed(
             ConditionalCommand(
                 openClawCommand,
                 closeClawCommand,
@@ -108,27 +100,10 @@ class OpenMainTeleOp : CommandOpMode() {
             )
         )
 
-        clawTrigger.toggleWhenActive(
-            InstantCommand({
-                slidesClawSubsystem.closeClaw()
-                slidesClawSubsystem.clawState = SlidesConst.ClawState.CLOSE
-            }, slidesClawSubsystem)
-        )
-
         // Register Subsystems
         register(driveSubsystem, slidesClawSubsystem)
 
         // Assign Default Commands
         driveSubsystem.defaultCommand = driveCommand
-
-        schedule(
-            PerpetualCommand(
-                RunCommand({
-                    telemetry.addData("Red: ", colorSensor.red())
-                    telemetry.addData("Distance: ", colorSensor.getDistance(DistanceUnit.CM))
-//                    telemetry.update()
-                })
-            ),
-        )
     }
 }
